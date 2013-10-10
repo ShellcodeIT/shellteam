96853ff56d6ddde2e5cf77cd2c9f0275c6f84cfc
Anderson Calixto <andersonbr@gmail.com>
2013-10-10 00:55:57 -0300

    joining rooms, message sending

---
+++ js/jquery.cookie.js
@@ -0,0 +1,84 @@
+/*
+ * jQuery Cookie Plugin v1.3.1
+ * Modified
+ * https://github.com/carhartl/jquery-cookie
+ */
+(function(factory) {
+	if (typeof define === 'function' && define.amd) {
+		// AMD. Register as anonymous module.
+		define([ 'jquery' ], factory);
+	} else {
+		// Browser globals.
+		factory(jQuery);
+	}
+}(function($) {
+	var config = {};
+	config.defaults = {path: "/"};
+	$.cookie = function(key, value, options) {
+		// write
+		if (value !== undefined) {
+			options = $.extend({}, config.defaults, options);
+			if (typeof options.expires === 'number') {
+				var days = options.expires, t = options.expires = new Date();
+				t.setDate(t.getDate() + days);
+			}
+			value = config.json ? JSON.stringify(value) : String(value);
+			return (document.cookie = [
+					config.raw ? key : encodeURIComponent(key),
+					'=',
+					config.raw ? value : encodeURIComponent(value),
+					options.expires ? '; expires='
+							+ options.expires.toUTCString() : '',
+					options.path ? '; path=' + options.path : '',
+					options.domain ? '; domain=' + options.domain : '',
+					options.secure ? '; secure' : '' ].join(''));
+		}
+		// read
+		var decode = config.raw ? raw : decoded;
+		var cookies = document.cookie.split('; ');
+		var result = key ? undefined : {};
+		for ( var i = 0, l = cookies.length; i < l; i++) {
+			var parts = cookies[i].split('=');
+			var sname = decode(parts.shift());
+			var cookie = decode(parts.join('='));
+
+			if (key && key === sname) {
+				result = converted(cookie);
+				break;
+			}
+
+			if (!key) {
+				result[sname] = converted(cookie);
+			}
+		}
+
+		return result;
+	};
+	function raw(s) {
+		return s;
+	}
+	function decoded(s) {
+		return decodeURIComponent(s.replace(/\+/g, ' '));
+	}
+	function converted(s) {
+		if (s.indexOf('"') === 0) {
+			// This is a quoted cookie as according to RFC2068, unescape
+			s = s.slice(1, -1).replace(/\\"/g, '"').replace(/\\\\/g, '\\');
+		}
+		try {
+			return config.json ? JSON.parse(s) : s;
+		} catch (er) {
+		}
+	}
+	$.removeCookie = function(key, options) {
+		if ($.cookie(key) !== undefined) {
+			// Must not alter options, thus extending a fresh object...
+			$.cookie(key, '', $.extend({}, options, {
+				expires : -1
+			}));
+			return true;
+		}
+		return false;
+	};
+
+}));
--- server.js
+++ server.js
@@ -6,9 +6,14 @@ app.listen(9999);
 
 var resources = {
 	'/': {file: '/index.html', mime: 'text/html'},
+	'/js/jquery-2.0.3.min.js': {file: '/js/jquery-2.0.3.min.js', mime: 'text/javascript'},
+	'/js/jquery.cookie.js': {file: '/js/jquery.cookie.js', mime: 'text/javascript'},
 	'/css/style.css': {file: '/css/style.css', mime: 'text/css'}
 };
 
+/*
+ * HTTP request handler
+ */
 function handler (req, res) {
 	var file = null;
 	var mime = null;
@@ -21,16 +26,12 @@ function handler (req, res) {
 		function (err, data) {
 			if (err) {
 				res.writeHead(500);
-				return res.end('Error loading index.html');
+				return res.end('Error loading ' + file);
 			}
 			res.writeHead(200, {"Content-Type": mime});
 			res.end(data);
 		});
 	}
-}
-
-var services = {
-	"fts_arquivo": 1.0
 }
 
 function chk (v) {
@@ -39,17 +40,79 @@ function chk (v) {
 	return false;
 }
 
+var users = {
+	"anderson": {
+		name: "Anderson Calixto",
+		password: "123",
+		activity: "none"
+	},
+	"admin": {
+		name: "Administrador",
+		password: "123",
+		activity: "none"
+	}
+}
+
+var rooms = {
+	"hardware": {
+		title: "Sala de mensagens referente à equipe de HARDWARE"
+	},
+	"software": {
+		title: "Sala de mensagens referente à equipe de SOFTWARE"
+	}
+}
+
+/*
+ * socket.io handler
+ */
 io.sockets.on('connection', function (socket) {
 	socket.__clientinfo = {};
+
+	/*
+	 * identification
+	 */
 	socket.on('identify', function (data) {
-		console.log("Cliente "+socket.id+" enviou pedido de identificacao");
+		var success = false;
+		console.log("Client "+socket.id+" has sent an identification request");
 		socket.__clientinfo.identity = data;
-		if (chk(data) && chk(data.service)) {
-			socket.emit("identified", "ok");
-			console.log("resultado da identificacao: ok");
-		} else {
-			socket.emit("identified", "fail");
-			console.log("resultado da identificacao: fail");
+		if (chk(data) && chk(data.user) && chk(data.password)) {
+			if (chk(users[data.user]) && data.password == users[data.user].password) {
+				success = true;
+				var user = users[data.user];
+				socket.join('work');
+				// alert anothers that has joined
+				socket.broadcast.to('work').emit('userJoin', {
+					user: data.user,
+					name: user.name,
+					activity: user.activity
+				});
+				socket.emit("identified", {
+					user: {
+						user: data.user,
+						name: user.name,
+						activity: user.activity
+					},
+					ret: "success"
+				});
+				console.log(socket.id+" identify: success");
+			}
+		}
+
+		if (!success) {
+			socket.emit("identified", {ret: "fail"});
+			console.log(socket.id+" identify: fail");
 		}
 	});
+	
+	socket.on('roomMessage', function(data) {
+		var user = users[socket.__clientinfo.identity.user];
+		socket.broadcast.to('work').emit('roomMessage', {
+			sender: {
+				user: socket.__clientinfo.identity.user,
+				name: user.name,
+				activity: user.activity
+			},
+			message: data
+		});
+	})
 });
